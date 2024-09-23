a1ef223ae2e4c7b1c343135983765f00b9253d7d
TMoiseyenko06 <tmoiseyenko06@gmail.com>
2024-09-23 11:51:43 -0400

    Fixed assignment

--- python_dict.py
+++ python_dict.py
@@ -1,7 +1,6 @@
 #Task 1
 def merge_dict(dict1,dict2):
     dict2.update(dict1)
-    print(dict2)
     return dict2
 
 #Task 2
@@ -20,4 +19,15 @@ def frequency(arr):
     return result
 
 
+dict1 = {1:1,2:2}
+dict2 = {1:1,3:3}
+array = [1,1,2,4,6,5,3,2,45,6,7,5]
 
+#Time complexitty is O(n) where n is the number of items in the first dictionary
+print(merge_dict(dict1,dict2))
+
+#Time complexity is also O(n) where n is the number of item in dict1
+print(intersect_dict(dict1,dict2))
+
+#Also O(n) time becuase it has to itterate through the given array, and the time complexity of adding the items to the dictionary is O(1) because of the unique keys
+print(frequency(array))
--- python_lists.py
+++ python_lists.py
@@ -12,3 +12,15 @@ def merge_lists(arr1,arr2):
     arr = arr1 + arr2
     return sorted(arr)
 
+
+list = [1,2,3,4,5,6]
+list2 = [5,7,12,6,3]
+
+#O(n) because it is itterating over the list
+print(square_list(list))
+
+#O(n) where n is the number of items in the provided sublist 
+print(reverse_sublist(list,2,5))
+
+#O(n log n) where n ais the combined number of items in both lists, also the list is sorted at the end so thats where the log comes in becuase timsort is O(n log n) time
+print(merge_lists(list,list2))
